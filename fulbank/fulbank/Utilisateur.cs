c46e2dd339482cc91b48df348df4d3f64c456bcc
hugorouff <hugo.rouff@lyceefulbert.fr>
2024-11-19 09:04:56 +0100

    modification-19/11

--- fulbank/fulbank/Utilisateur.cs
+++ fulbank/fulbank/Utilisateur.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-
+using MySqlConnector;
 namespace fulbank
 {
     internal class Utilisateur
@@ -12,26 +13,51 @@ namespace fulbank
 
 
         private int id;
+        private List<int> numComptes;
+        private List<float> soldes;
+        private List<string> typeComptes;
+        private List<string> monnaies;
 
-        Utilisateur(int idUtil ) 
-        { 
+        Utilisateur(int idUtil)
+        {
             this.id = idUtil;
+            this.numComptes = new List<int>();
+            this.soldes = new List<float>();
+            this.typeComptes = new List<string>();
+            this.monnaies = new List<string>();
         }
 
-        public int getId()  { return id; }
+        public int getId() { return id; }
+        public static Utilisateur getInstance()
+        {
+            return Utilisateur.utilActuel;
+        }
 
-        public static void NewInstance(int idUtil) 
+        public static void NewInstance(int idUtil)
         {
             Utilisateur.utilActuel = new Utilisateur(idUtil);
         }
 
-        public static Utilisateur getInstance()
+        public static Utilisateur getUser()
         {
-            if (utilActuel == null)
+            return Utilisateur.utilActuel;
+        }
+
+        public void PullComptes()
+        {
+            MySqlConnection BDD = ConnexionBDD.Connexion();
+            MySqlCommand cmd = new MySqlCommand("select numeroCompte,typeCompte,solde,Monnaie from comptes_utilisateur where titulaire = @id_ or coTitulaire = @id_ ;", BDD);
+            cmd.Parameters.Add(new MySqlParameter("id_", this.id));
+            BDD.Open();
+            MySqlDataReader data = cmd.ExecuteReader();
+
+            while (data.Read())
             {
-                throw new InvalidOperationException("L'utilisateur n'a pas été initialisé. Appelez Utilisateur.NewInstance() avant d'utiliser cette méthode.");
+                this.numComptes.Add(data.GetInt32(0));
+                this.soldes.Add(data.GetFloat(2));
+                this.typeComptes.Add(data.GetString(1));
+                this.monnaies.Add(data.GetString(3));
             }
-            return utilActuel;
         }
     }
 }
